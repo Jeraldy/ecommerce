e2a3daadeee16a4c9c2dfcfcd4907a7740cc288a
jeraldy <deusjeraldy@gamil.com>
2023-01-06 14:15:35 +0300

    docker

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,31 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const app = require('./app')
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// const app = require('./app')
 
-dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT || 3000;
-const database = process.env.DATABASE
+// const port = process.env.PORT || 3000;
+// const database = process.env.DATABASE
 
-mongoose.connect(database, {})
-    .then(_ => console.log("Db connection was successfuly"))
-    .catch(err => console.log(err));
+// mongoose.connect(database, {})
+//     .then(_ => console.log("Db connection was successfuly"))
+//     .catch(err => console.log(err));
 
-app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+// app.listen(port, () => {
+//     console.log(`App is running on port ${port}`);
+// });
+
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
 });
