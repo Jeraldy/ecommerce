6728a16278bff58348354e847c5eb09689b37831
jeraldy <deusjeraldy@gamil.com>
2023-01-19 12:11:28 +0300

    mongo-doecker configs

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,14 @@
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-// const app = require('./app')
+const mongoose = require('mongoose');
+const app = require('./app')
 
-// dotenv.config({ path: './config.env' });
+const port = process.env.PORT || 8080;
+const database = process.env.DATABASE;
 
-// const port = process.env.PORT || 3000;
-// const database = process.env.DATABASE
 
-// mongoose.connect(database, {})
-//     .then(_ => console.log("Db connection was successfuly"))
-//     .catch(err => console.log(err));
+mongoose.connect(database, { useNewUrlParser: true })
+    .then(_ => console.log("Db connection was successfuly"))
+    .catch(err => console.log(err));
 
-// app.listen(port, () => {
-//     console.log(`App is running on port ${port}`);
-// });
-
-const express = require('express');
-
-const PORT = process.env.PORT || 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
 });
