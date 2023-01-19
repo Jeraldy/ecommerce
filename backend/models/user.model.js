612b10ad0e4252586e19d01bc51f5e1d8f26bd39
jeraldy <deusjeraldy@gamil.com>
2023-01-19 14:07:44 +0300

    created dev branch

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -8,38 +8,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please tell us your name!']
   },
-//   email: {
-//     type: String,
-//     required: [true, 'Please provide your email'],
-//     unique: true,
-//     lowercase: true,
-//     validate: [validator.isEmail, 'Please provide a valid email']
-//   },
-//   photo: String,
-//   role: {
-//     type: String,
-//     enum: ['user', 'admin'],
-//     default: 'user'
-//   },
-//   password: {
-//     type: String,
-//     required: [true, 'Please provide a password'],
-//     minlength: 8,
-//     select: false
-//   },
-//   passwordConfirm: {
-//     type: String,
-//     required: [true, 'Please confirm your password'],
-//     validate: {
-//       validator: function(val) {
-//         return val === this.password;
-//       },
-//       message: 'Passwords are not the same!'
-//     }
-//   },
-//   passwordChangedAt: Date,
-//   passwordResetToken: String,
-//   passwordResetExpires: Date,
   createdAt: {
     type: Date,
     default: Date.now()
@@ -50,67 +18,6 @@ const UserSchema = new mongoose.Schema({
     select: false
   }
 });
-
-// UserSchema.pre('save', async function(next) {
-//   // Only run this function if password was actually modified
-//   if (!this.isModified('password')) return next();
-
-//   // Hash the password with cost of 12
-//   this.password = await bcrypt.hash(this.password, 12);
-
-//   // Delete passwordConfirm field
-//   this.passwordConfirm = undefined;
-//   next();
-// });
-
-// UserSchema.pre('save', function(next) {
-//   if (!this.isModified('password') || this.isNew) return next();
-
-//   this.passwordChangedAt = Date.now() - 1000;
-//   next();
-// });
-
-// UserSchema.pre(/^find/, function(next) {
-//   // this points to the current query
-//   this.find({ active: { $ne: false } });
-//   next();
-// });
-
-// UserSchema.methods.correctPassword = async function(
-//   candidatePassword,
-//   userPassword
-// ) {
-//   return await bcrypt.compare(candidatePassword, userPassword);
-// };
-
-// UserSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
-//   if (this.passwordChangedAt) {
-//     const changedTimestamp = parseInt(
-//       this.passwordChangedAt.getTime() / 1000,
-//       10
-//     );
-
-//     return JWTTimestamp < changedTimestamp;
-//   }
-
-//   // False means NOT changed
-//   return false;
-// };
-
-// UserSchema.methods.createPasswordResetToken = function() {
-//   const resetToken = crypto.randomBytes(32).toString('hex');
-
-//   this.passwordResetToken = crypto
-//     .createHash('sha256')
-//     .update(resetToken)
-//     .digest('hex');
-
-//   console.log({ resetToken }, this.passwordResetToken);
-
-//   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
-//   return resetToken;
-// };
 
 const User = mongoose.model('User', UserSchema);
 
