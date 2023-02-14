52246a96ce6886d4a20d86916f3541665ee1007b
jeraldy <deusjeraldy@gamil.com>
2023-02-14 14:59:18 +0300

    farget deployment test

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(express.json());
 app.use('/api/v1/user', userRouter);
 
+app.use('/health', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        statusDesc: 'Api service is up and running'
+    });
+});
+
 app.all('*', (req, res, next) => {
     const message = `Ooops Can't find ${req.originalUrl} on this server.`;
     next(new AppError(message, 404));
