7ea3aaed5974d07d51440a61eebda592640ce2c2
jeraldy <deusjeraldy@gamil.com>
2023-02-16 13:40:27 +0300

    code cleaning

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,16 @@ const app = require('./app')
 const port = process.env.PORT || 8080;
 const env = process.env.NODE_ENV || 'local';
 
-let databaseConnection = process.env.DATABASE_LOCAL;
+let databaseConnection = '';
 if(env === 'production'){
     databaseConnection = process.env.DATABASE_PROD;
 }else if(env === 'development'){
     databaseConnection = process.env.DATABASE_DEV;
 }
 
-mongoose.connect(databaseConnection, { useNewUrlParser: true })
-    .then(_ => console.log("Db connection was successfuly"))
-    .catch(err => console.log(err));
+// mongoose.connect(databaseConnection, { useNewUrlParser: true })
+//     .then(_ => console.log("Db connection was successfuly"))
+//     .catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
