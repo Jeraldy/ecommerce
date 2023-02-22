8ca986a5b65827e257895bc2ac0693675386bf87
jeraldy <deusjeraldy@gamil.com>
2023-02-22 18:40:27 +0300

    added db connection

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,21 @@ const mongoose = require('mongoose');
 const app = require('./app')
 
 const port = process.env.PORT || 8080;
-const env = process.env.NODE_ENV || 'local';
+const env = process.env.NODE_ENV;
 
-let databaseConnection = '';
-if(env === 'production'){
-    databaseConnection = process.env.DATABASE_PROD;
-}else if(env === 'development'){
-    databaseConnection = process.env.DATABASE_DEV;
+let dbConnection = process.env.DATABASE_DEV;
+if(env === 'prod'){
+    dbConnection = process.env.DATABASE_PROD;
+}else if(env === 'test'){
+    dbConnection = process.env.DATABASE_TEST;
 }
 
-// mongoose.connect(databaseConnection, { useNewUrlParser: true })
-//     .then(_ => console.log("Db connection was successfuly"))
-//     .catch(err => console.log(err));
+mongoose.connect(dbConnection, { useNewUrlParser: true })
+    .then(_ => {
+        console.log("Db connection was successfuly")
+        console.log(`NODE_ENV: ${env}, DB: ${dbConnection}`)
+    })
+    .catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
