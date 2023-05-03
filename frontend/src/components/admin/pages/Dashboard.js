8124e25d079c98496ea5f4b7a24517f800cd6b4a
Joseph Martin <joemartine701@gmail.com>
2023-05-03 17:47:48 +0300

    list of users

--- frontend/src/components/admin/pages/Dashboard.js
+++ frontend/src/components/admin/pages/Dashboard.js
@@ -1,8 +1,13 @@
+import { Card } from "antd";
 import React from "react";
 
 function Dashboard (){
   return(
+    <>
+    <Card>
     <h1>Dashboard</h1>
+    </Card>
+    </>
   )
 }
 export default Dashboard;
